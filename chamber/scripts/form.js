fc8cf80ad8608622522b5ef2f6c5d53febb55704
Hgregori <Hgregorio@byupathway.edu>
2026-03-23 12:47:00 -0300

    fixing

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -1,1 +1,6 @@
+const homeBttn = document.getElementById('home-button');
+homeBttn.addEventListener('click', () => {
+  window.location.href = 'index.html';
+});
+
 document.getElementById("timestamp").value = new Date().toISOString();
