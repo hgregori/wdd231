87914a83d7d19c70cd304ed665573eaba83cf6c6
Hgregori <Hgregorio@byupathway.edu>
2026-03-31 12:05:15 -0300

    W6 project hamburger

---
+++ project/scripts/hamburger.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const mobileNav = document.getElementById("mobile-nav");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileNav.classList.toggle("open");
+});
